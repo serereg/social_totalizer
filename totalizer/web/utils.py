0e0a8958f3038e427401691d4e55eb0f5ba58122
serereg <serereg@mail.ru>
2021-06-28 18:47:58 +0300

    docstring fixed

--- totalizer/web/utils.py
+++ totalizer/web/utils.py
@@ -4,17 +4,16 @@ from typing import List, Dict
 
 
 def form_csv(file, columns: List[str], rows: List[Dict]):
-    """Forming csv file from given columns and
+    """Forming csv file-like object from given columns and
         list of rows as dictionaries.
 
     Args:
-        file: file to be created.
+        file: file-like object.
         columns: columns for resulting table for csv file.
         rows: list with dictionaries. For example:
             [{"col1": time1, "col2": value1},
             {"col1" time2, "col2": value2}, ...]
     """
-    # with file.open(mode="w", newline="") as csvfile:
     writer = csv.DictWriter(file, fieldnames=columns)
     writer.writeheader()
     for row in rows:
